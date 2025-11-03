7c405ae8b39d45b97e8c3e656d33fb681fba9617
Martin Nyholm Jelle <mjelle@hotmail.com>
2025-11-03 22:49:08 +0100

    fix import

--- runtime/src/main/java/org/mjelle/quarkus/easynats/NatsPublisher.java
+++ runtime/src/main/java/org/mjelle/quarkus/easynats/NatsPublisher.java
@@ -9,7 +9,7 @@ import io.nats.client.JetStream;
 import io.nats.client.JetStreamApiException;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Scope;
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import org.mjelle.quarkus.easynats.codec.Codec;
 import org.mjelle.quarkus.easynats.codec.SerializationException;
